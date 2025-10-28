5cc5acf89322736160c2168bf0b5743ad7367110
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-10-28 19:29:45 +0000

    did not know there was a difference between ' and `

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let firstName = 'Addy';
 let jobTitle = 'UX Designer';
 let city = 'Maquoketa';
 
-let newJobPost = 'Dear ${firstName},
+let newJobPost = `Dear ${firstName},
     Thank you for applying for our recent ${jobTitle} position in ${city}.
 We will review your application and get back to you shortly.
 
-Sincerly,
-Whoever'
+Sincerely,
+Whoever`;
 
 document.getElementById("result").innerHTML = newJobPost;
